a8e1f22d9faf54a708d26b8a631abb763cbf0bb1
JesperSqv <jesper.sundqvist@netikka.fi>
2023-05-31 15:55:54 +0300

    Added a listController to control the lists

---
+++ shopping-lists/controllers/listController.js
@@ -0,0 +1,26 @@
+import { renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import * as listService from "../services/listService.js";
+import * as requestUtils from "../utils/requestUtils.js";
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
+
+const addList = async (request) => {
+  const formData = await request.formData();
+  const name = formData.get("name");
+
+  await listService.create(name);
+
+  return redirectTo("/lists");
+};
+
+const viewLists = async (request) => {
+  const data = {
+    lists: await listService.findAllActiveShoppingLists(),
+  };
+
+  return new Response(await renderFile("lists.eta", data), responseDetails);
+};
+
+export { addList, viewLists };
