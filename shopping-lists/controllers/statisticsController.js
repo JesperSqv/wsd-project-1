8edf1f6da22af1cfcdb112fa29c1d183a94c654a
JesperSqv <jesper.sundqvist@netikka.fi>
2023-06-08 14:55:26 +0300

    Staring on the main page

---
+++ shopping-lists/controllers/statisticsController.js
@@ -0,0 +1,5 @@
+import { renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+import * as itemService from "../services/itemService.js";
+import * as listService from "../services/listService.js";
+import * as requestUtils from "../utils/requestUtils.js";
+
