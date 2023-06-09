6cbfc9529a828dc6815979d924cff9f2f6968e59
JesperSqv <jesper.sundqvist@netikka.fi>
2023-06-09 10:07:25 +0300

    Working on tests, fixed main.eta if no lists

---
+++ e2e-playwright/tests/main-page.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require("@playwright/test");
+
+test("Main page has expected title, headings and paragraph.", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle("Shared shopping lists");
+  await expect(page.locator("h1")).toHaveText("Shared shopping lists");
+  await expect(page.locator("p")).toHaveText("No shopping lists yet.");
+});
